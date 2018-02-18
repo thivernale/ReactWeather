ca458043f9dc52e5d407b9d934057d9b0bbff54f
thivernale <rainm000n@gmail.com>
2018-02-18 16:24:04 +0200

    Prep repo for Heroku

--- server.js
+++ server.js
@@ -7,6 +7,22 @@ var express = require('express');
 // create a new app:
 // call express library as a function with no args
 var app = express();
+// get port from environment (on Heroku) or specify default one (for local use)
+// In ES6 and Node we can use "const" keyword to define constants
+const PORT = process.env.PORT || 3000;
+
+// we are redirecting https requests to weather API over http
+// a common pattern for express middleware (lets us do something with every request)
+app.use(function(req, res, next) {
+    // we are going to redirect traffic if it is over HTTPS
+    if (req.headers['x-forwarded-proto'] === 'http') {
+        // call next function, do nothing
+        next();
+    } else {
+        // redirect to same URL over HTTP
+        res.redirect('http://' + req.hostname + req.url);
+    }
+});
 
 // tell the server which folder to serve
 // 'use' adds functionality to an Express app
@@ -15,6 +31,6 @@ app.use(express.static('public'));
 
 // start the server:
 // specify port and a function that gets called when the server is up
-app.listen(3000, function() {
-    console.log('Express server is up on port 3000');
+app.listen(PORT, function() {
+    console.log('Express server is up on port ' + PORT);
 });
