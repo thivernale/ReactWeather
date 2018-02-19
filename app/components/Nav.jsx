edc08d2f225ff713253c709e96e245e8b5008ff3
thivernale <rainm000n@gmail.com>
2018-02-20 00:29:58 +0200

    Styled nav bar

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -15,15 +15,56 @@ var { Link, IndexLink } = require('react-router');
     }
 });*/
 
-var Nav = (props) => {
-    return (
-        <div style={{ border: "1px solid #ff6600" }}>
-            <h2>Nav Component</h2>
-            <IndexLink to="/" activeClassName="active" activeStyle={{ fontWeight: 'bold' }}>Get Weather</IndexLink>
-            <Link to="/about" activeClassName="active" activeStyle={{ fontWeight: 'bold' }}>About</Link>
-            <Link to="/examples" activeClassName="active" activeStyle={{ fontWeight: 'bold' }}>Examples</Link>
-        </div>
-    );
-};
+// now we use foundation styling
+// but we use "className" attrubute instead of "class" because otherwise there will be a collision with the jsx property with the same name
+var Nav = React.createClass({
+    onSearch: function(e) {
+        e.preventDefault();
+        alert('Not yet wired up!');
+    },
+    render: function() {
+        return (
+            <div className="top-bar">
+                <div className="top-bar-left">
+                    <ul className="menu">
+                        <li className="menu-text">
+                            React Weather App
+                        </li>
+                        <li>
+                            <IndexLink to="/" activeClassName="active" activeStyle={{ fontWeight: 'bold' }}>Get Weather</IndexLink>
+                        </li>
+                        <li>
+                            <Link to="/about" activeClassName="active" activeStyle={{ fontWeight: 'bold' }}>About</Link>
+                        </li>
+                        <li>
+                            <Link to="/examples" activeClassName="active" activeStyle={{ fontWeight: 'bold' }}>Examples</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="top-bar-right">
+                    <form onSubmit={this.onSearch}>
+                        <ul className="menu">
+                            <li>
+                                <input type="search" placeholder="Search weather" />
+                            </li>
+                            <li>
+                                <input type="submit" className="button" value="Get Weather" />
+                            </li>
+                        </ul>
+                    </form>
+                </div>
+            </div>
+        );
+    }
+});
 
 module.exports = Nav;
+
+/*var old = (
+    <div style={{ border: "1px solid #ff6600" }}>
+        <h2>Nav Component</h2>
+        <IndexLink to="/" activeClassName="active" activeStyle={{ fontWeight: 'bold' }}>Get Weather</IndexLink>
+        <Link to="/about" activeClassName="active" activeStyle={{ fontWeight: 'bold' }}>About</Link>
+        <Link to="/examples" activeClassName="active" activeStyle={{ fontWeight: 'bold' }}>Examples</Link>
+    </div>
+);*/
