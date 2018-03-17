174deab1a083084c4ed905d550bd12ec6e09e5ee
thivernale <rainm000n@gmail.com>
2018-03-18 00:57:19 +0200

    Fix error modal bug

--- app/components/ErrorModal.jsx
+++ app/components/ErrorModal.jsx
@@ -1,4 +1,6 @@
 var React = require('react');
+var ReactDOM = require('react-dom');
+var ReactDOMServer = require('react-dom/server');
 
 var ErrorModal = React.createClass({
     // error title will be optional, provide default one:
@@ -19,16 +21,9 @@ var ErrorModal = React.createClass({
     // The componentDidMount() hook runs after the component output has been rendered to the DOM.
     // @see https://reactjs.org/docs/state-and-lifecycle.html
     componentDidMount: function() {
-        // create a new instance of a Modal:
-        // pass either a DOM id (using jquery selector here) or a ref as a parameter
-        var modal = new Foundation.Reveal($('#error-modal'));
-        // show the modal:
-        modal.open();
-    },
 
-    render: function() {
         var { title, message } = this.props;
-        return (
+        var modalMarkup = (
             <div id="error-modal" className="reveal tiny text-center" data-reveal="">
                 <h4>{title}</h4>
                 <p>{message}</p>
@@ -37,6 +32,31 @@ var ErrorModal = React.createClass({
                         OK
                     </button>
                 </p>
+            </div>
+        );
+
+        // now we have to add this markup into the dom
+
+        // create variable with $ - because it is going to be a jquery selector
+        // first render JSX code to sting, then pass it to create the contents
+        var $modal = $(ReactDOMServer.renderToString(modalMarkup));
+
+        // add  to the component
+        // find the component DOM node, then use jQuery html method to pass the markup to show to the screen
+        $(ReactDOM.findDOMNode(this)).html($modal);
+
+        // create a new instance of a Modal:
+        // pass either a DOM id (using jquery selector here) or a ref as a parameter
+        var modal = new Foundation.Reveal($('#error-modal'));
+        // show the modal:
+        modal.open();
+    },
+
+    render: function() {
+
+        // return an empty div - component renders nothing because Foundation is going to manipulate the DOM so we start with no DOM at all
+        return (
+            <div>
             </div>
         );
     }
